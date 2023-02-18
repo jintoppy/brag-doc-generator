d1a01ab058f69f3e43a2666c7500b6541a8945f2
Vaibhav Arora <vaibhav.arora@snorkel.ai>
2023-02-18 21:09:19 +0530

    feat: add UI with form

--- web-app/src/App.js
+++ web-app/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useForm } from "react-hook-form";
+import { questions } from "./data";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import { useState } from "react";
 
 function App() {
+  const [answers, setAnswers] = useState({});
+
+  const updateAnswer = (answer, questionIndex) => {
+    setAnswers((newAnswers) => [{ ...newAnswers, questionIndex: answer }]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ margin: 20 }}>
+      <h1 style={{ marginBottom: 30 }}>Brag graph generator</h1>
+      {questions.map((question, index) => (
+        <QnA
+          question={question}
+          updateAnswer={(answer) => updateAnswer(answer, index)}
+        />
+      ))}
+      <Button variant="contained">Submit</Button>
     </div>
   );
 }
+
+const QnA = ({ question, updateAnswer }) => {
+  return (
+    <div style={{ width: "100%" }}>
+      <TextField
+        onChange={(event) => updateAnswer(event.target.value)}
+        fullWidth
+        label={question}
+        variant="outlined"
+        sx={{ mb: 2 }}
+      />
+    </div>
+  );
+};
 
 export default App;
---
+++ web-app/src/data.js
@@ -0,0 +1,8 @@
+const questions = [
+  "Did you achieve your OKR?",
+  "Did you promote a positive culture within the team and outside the team?",
+  "What was your most significant achievement from last year?",
+  "What you could have done better in last year?",
+];
+
+export { questions };
